eb4a1afac128beee8330d6bd278333ef68cba92d
serhii <chernishsache@gmail.com>
2024-09-07 13:09:55 +0200

    создание xml

--- dreams.rb
+++ dreams.rb
@@ -1,14 +1,21 @@
 require "rexml/document"
+
+file_path = (__dir__ + "/dr.xml")
+unless File.exist?(file_path)
+  xmlfile = File.open(file_path, "w") do |file|
+    file.puts "<?xml version='1.0' encoding='UTF-8'?>"
+    file.puts "<wishes></wishes>"
+  end
+end
+xmlfile = File.read(file_path)
+xmldoc = REXML::Document.new(xmlfile)
 puts "В этом сундуке хранятся ваши желания.
 Чего бы вы хотели?"
+
 dream = STDIN.gets.chomp
 puts "До какой даты вы бы хотели это осуществить"
 data = STDIN.gets.chomp
-file_path = (__dir__ + "/dr.xml")
-xmlfile = File.new(file_path, "r")
 
-xmldoc = REXML::Document.new(xmlfile)
-xmlfile.close
 wishes = xmldoc.root
 wish = wishes.add_element("wish", { "date" => data })
 wish.text = dream
