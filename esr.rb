8ce946f97d93cfa9960e456a403e47b3dc067a46
serhii <chernishsache@gmail.com>
2024-09-07 13:09:04 +0200

    создание файла при отсутствии

--- esr.rb
+++
@@ -1,9 +0,0 @@
-require "builder"
-xml = Builder::XmlMarkup.new(indent: 2)
-xml.instruct! :xml, version: "1.0", encoding: "UTF-8"
-xml.person do
-  xml.name "jon doooo"
-  xml.age 40
-  xml.email "jo@doo"
-end
-puts xml.target!
